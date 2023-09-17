cb0e2663ce6bca09c018b7b6e74f12680ce53a83
Eduard Mednikov <EduardMedh@yandex.ru>
2023-09-17 15:48:35 +0300

    Рабочий слайдер

---
+++ src/scripts/typescript/slider.ts
@@ -0,0 +1,46 @@
+function showSlide(index: number, buttons:NodeListOf<Element>){
+    if(index < 0){
+        index = 2;
+    }
+    else if(index > 2){
+        index = 0;
+    };
+
+    let slides = document.querySelectorAll(".section__banner");
+
+    slides.forEach((slide, i) => {
+        if (i !== index) {
+            slide.classList.add("section__banner--hide");
+            buttons[i].classList.remove("switch--active");
+        }
+    });
+    
+    slides[index].classList.remove("section__banner--hide");
+    buttons[index].classList.add("switch--active");
+}
+
+function showFirstSlide(){
+    showSlide(0, switches);
+}
+
+const switches = document.querySelectorAll(".switch");
+
+switches[0].addEventListener('click', () => {
+    showSlide(0, switches);
+});
+
+switches[1].addEventListener('click', () => {
+    showSlide(1, switches);
+});
+
+switches[2].addEventListener('click', () => {
+    showSlide(2, switches);
+});
+
+showFirstSlide();
+
+let index: number = 0;
+setInterval(() => {
+    index = index > 2 ? 0 : index
+    showSlide(index++, switches)
+}, 5000);
